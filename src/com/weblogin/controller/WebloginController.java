6771ac9b64f91f183f9721545d747c00837c87ce
Yashpal Choudhary <yash.choudhary@gmail.com>
2015-02-03 17:38:14 +0000

    weblogin files

---
+++ src/com/weblogin/controller/WebloginController.java
@@ -0,0 +1,17 @@
+package com.weblogin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class WebloginController {
+	@RequestMapping("/welcome")
+	public ModelAndView helloWorld() {
+ 
+		String message = "<br><div align='center'>"
+				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is comming from CrunchifyHelloWorld.java **********<br><br>";
+		return new ModelAndView("welcome", "message", message);
+	}
+
+}
